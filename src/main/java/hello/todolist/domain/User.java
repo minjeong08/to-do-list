8a89644e0cb7904431ad61e527f65f949e5d56fb
minnjung0887921 <minjeong0887921@gmail.com>
2024-12-02 21:45:51 +0900

    test : user 엔티티 연관메서드

--- src/main/java/hello/todolist/domain/User.java
+++ src/main/java/hello/todolist/domain/User.java
@@ -26,6 +26,15 @@ public class User {
     List<Task> tasks = new ArrayList<>();
 
     public void addCategory(Category category) {
+
+        if (category.getUser() != null && category.getUser() != this) {
+            throw new IllegalStateException("카테고리 생성자와 일치하지 않습니다");
+        }
+
+        if (categories.stream().anyMatch(c -> c.getCateName().equals(category.getCateName()))) {
+            throw new IllegalArgumentException("이미 존재하는 카테고리 이름입니다.");
+        }
+
         categories.add(category);
         if (category.getUser() != this) {
             category.setUser(this);
@@ -33,11 +42,20 @@ public class User {
     }
 
     public void removeCategory(Category category) {
+
+        if (!categories.contains(category) || category == null || category.getUser() != this) {
+            throw new IllegalStateException("존재하지 않는 카테고리입니다.");
+        }
+
         categories.remove(category);
         category.setUser(null);
     }
 
     public void addTask(Task task) {
+        if (task.getUser() != null && task.getUser() != this) {
+            throw new IllegalStateException("잘못된 접근입니다.");
+        }
+
         tasks.add(task);
         if (task.getUser() != this) {
             task.setUser(this);
@@ -45,6 +63,10 @@ public class User {
     }
 
     public void removeTask(Task task) {
+        if (!tasks.contains(task) || task == null || task.getUser() != this) {
+            throw new IllegalStateException("잘못된 접근입니다.");
+        }
+
         tasks.remove(task);
         task.setUser(null);
     }
---
+++ src/test/java/hello/todolist/domain/UserTest.java
@@ -0,0 +1,176 @@
+package hello.todolist.domain;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class UserTest {
+
+    @Autowired EntityManager em;
+
+    @Test
+    void 카테고리_추가_성공() {
+        // given
+        User user = new User();
+        Category category = new Category();
+        category.setCateName("test");
+
+        // when
+        user.addCategory(category);
+
+        // then
+        assertThat(user.getCategories()).contains(category);
+        assertThat(category.getUser()).isEqualTo(user);
+    }
+
+    @Test
+    void 설정된_유저와_불일치한_카테고리_추가() {
+        // given
+        User user1 = new User();
+        User user2 = new User();
+
+        Category category = new Category();
+        category.setUser(user2);
+
+        // when, then
+        assertThatThrownBy(() -> user1.addCategory(category))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("카테고리 생성자와 일치하지 않습니다");
+    }
+
+    @Test
+    void 중복_카테고리_추가() {
+        // given
+        User user = new User();
+
+        Category cate1 = new Category();
+        cate1.setCateName("카테고리");
+        Category cate2 = new Category();
+        cate2.setCateName("카테고리");
+
+        user.addCategory(cate1);
+
+        // when, then
+        assertThatThrownBy(() -> user.addCategory(cate2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이미 존재하는 카테고리 이름입니다.");
+    }
+
+    @Test
+    void 카테고리_삭제_성공() {
+        // given
+        User user = new User();
+        Category category = new Category();
+        user.addCategory(category);
+
+        // when
+        user.removeCategory(category);
+
+        // then
+        assertThat(user.getCategories()).doesNotContain(category);
+        assertThat(category.getUser()).isNull();
+    }
+
+    @Test
+    void 설정된_유저와_불일치한_카테고리_삭제() {
+        // given
+        User user1 = new User();
+        User user2 = new User();
+
+        Category category = new Category();
+        user2.addCategory(category);
+
+        // when, then
+        assertThatThrownBy(() -> user1.removeCategory(category))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("존재하지 않는 카테고리입니다.");
+    }
+
+    @Test
+    void 존재하지_않는_카테고리_삭제() {
+        // given
+        User user = new User();
+        Category category = new Category();
+
+        // when & then
+        assertThatThrownBy(() -> user.removeCategory(category))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("존재하지 않는 카테고리입니다.");
+    }
+
+    @Test
+    void 할일_추가_성공() {
+        // given
+        User user = new User();
+        Category category = new Category();
+        Task task = new Task();
+
+        task.setUser(user);
+        task.setCategory(category);
+
+        // when
+        user.addTask(task);
+
+        // then
+        assertThat(user.tasks).contains(task);
+        assertThat(task.getUser()).isEqualTo(user);
+        assertThat(task.getCategory()).isEqualTo(category);
+    }
+
+    @Test
+    void 설정된_유저가_불일치한_할일_추가() {
+        // given
+        User user1 = new User();
+        User user2 = new User();
+        Category category = new Category();
+        Task task = new Task();
+
+        task.setCategory(category);
+        task.setUser(user2);
+
+        // when & then
+        assertThatThrownBy(() -> user1.addTask(task))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("잘못된 접근입니다.");
+    }
+
+    @Test
+    void 할일_삭제_성공() {
+        // given
+        User user = new User();
+        Task task = new Task();
+        user.addTask(task);
+
+        // when
+        user.removeTask(task);
+
+        // then
+        assertThat(user.getTasks()).doesNotContain(task);
+        assertThat(task.getUser()).isNull();
+    }
+
+    @Test
+    void 설정된_유저와_불일치한_할일_삭제() {
+        // given
+        User user1 = new User();
+        User user2 = new User();
+
+        Task task = new Task();
+        user2.addTask(task);
+
+        // when & then
+        assertThatThrownBy(() -> user1.removeTask(task))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("잘못된 접근입니다.");
+    }
+}
